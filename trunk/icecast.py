e21f5b65ad520e51d88867125ad9e084f0644e71
coda.trigger <coda.trigger@fa9a1432-c74e-0410-877c-c7f5fb51d9ec>
2010-05-28 05:11:55 +0000

    using icecast-python again, let's hope it works better

--- trunk/icecast.py
+++ trunk/icecast.py
@@ -12,9 +12,9 @@ example:
 import socket
 import stream_settings
 import time
-import shoutpy
 from base64 import b64encode
 from urllib import urlencode
+import random
 
 # format a dict as HTTP request headers, but with configurable line endings
 # (since icecast wants \n instead of \r\n in some places)
@@ -23,23 +23,36 @@ def request_format(request, line_separator="\n"):
 
 def connect():
     global s, packets_sent, start_time
-    s = shoutpy.Shout()
-    s.host = stream_settings.host
-    s.port = stream_settings.port
-    s.password = stream_settings.password
-    s.mount = stream_settings.mount_point
-    s.format = shoutpy.FORMAT_MP3
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((stream_settings.host, stream_settings.port))
+    s.sendall("SOURCE %s ICE/1.0\n%s\n%s\n\n" % (
+        stream_settings.mount_point, 
+        request_format({
+            'content-type': 'audio/mpeg',
+            'Authorization': 'Basic ' + b64encode("source:" + stream_settings.password),
+            'User-Agent': stream_settings.user_agent
+        }),
+        request_format({
+            'ice-name': stream_settings.name,
+            'ice-url': stream_settings.url,
+            'ice-genre': stream_settings.genre,
+            'ice-bitrate': stream_settings.bitrate,
+            'ice-private': 0,
+            'ice-public': 1,
+            'ice-description': stream_settings.description,
+            'ice-audio-info': "ice-samplerate=%d;ice-bitrate=%d;ice-channels=%d" %
+                (stream_settings.samplerate, stream_settings.bitrate, stream_settings.channels)
+        })
+    ))
     
-    s.name = stream_settings.name
-    s.url = stream_settings.url
-    s.genre = stream_settings.genre
-    s.bitrate = stream_settings.bitrate
-    s.description = stream_settings.description
-    s.set_audio_info( "bitrate", str( stream_settings.bitrate ) )
-    s.set_audio_info( "samplerate", str( stream_settings.samplerate ) )
-    s.set_audio_info( "channels", str( stream_settings.channels ) )
-
-    s.open()
+    response = s.recv(4096)
+    if len(response) == 0:
+        raise "No response from icecast server"
+    if response.find(r"HTTP/1.0 200 OK") == -1:
+        raise "Server response: %s" % response
+    start_time = time.time()
+    packets_sent = 0
+    
         
 # update the metadata using a new socket -- i guess there's no other useful keys besides "song"
 def update_metadata(song):
@@ -62,25 +75,27 @@ def update_metadata(song):
 
 def send(buf):
     global s, packets_sent, start_time
-    recon_success = 0
-    try:
-        s.send(buf)
-        send_success = 1
-    except:
-        s.close()
-        while recon_success == 0:
-            try:
-                s.open()
-                recon_success = 1
-            except:
-                recon_success = 0
-                # nada
-    
-    # Before syncing, give other threads a chance to run
-    # Pythons threading is crap.
-    time.sleep( 0.00001 )
-    s.sync()
+    packet_start_time = time.time()
+    s.send(buf)
+    #time.sleep( 0.070 + random.random() * 0.1 ) # network simulator
+    packets_sent += 1
+    packet_elapsed_time = time.time() - packet_start_time
+    total_elapsed_time =  (time.time() - start_time)
+    # total packets needed = seconds * bits per second / bits per packet
+    packets_needed = int( total_elapsed_time * stream_settings.bitrate * 1000.0 / (4096 * 8) )
+    extra_packets = packets_sent - packets_needed
+
+    # return immediately if we're going too slow
+    #if extra_packets <= stream_settings.buffer_packets:
+    #    return
+    # sleep for however much time is remaining to meet our bitrate
+    total_packet_time = extra_packets * (4096.0 * 8) / (stream_settings.bitrate * 1000.0)
+    print "sleeping for %f, %d packets sent, %d packets needed" % ((total_packet_time - packet_elapsed_time), packets_sent, packets_needed)
+    if( total_packet_time - packet_elapsed_time < 0):
+    	time.sleep(0)
+    else:
+    	time.sleep(total_packet_time - packet_elapsed_time)
 
 def close():
-#    s.shutdown(1)
+    s.shutdown(1)
     s.close()
