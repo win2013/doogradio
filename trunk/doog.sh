c303bdc96782d4be1768351db77c4da6b59e2edd
nobuyuki.nyuu <nobuyuki.nyuu@fa9a1432-c74e-0410-877c-c7f5fb51d9ec>
2010-06-09 15:36:23 +0000

    shell script now loops in case of disconnects.  It also traps keyboard messages

--- trunk/doog.sh
+++ trunk/doog.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+cd .
 export LD_LIBRARY_PATH=.
-python radio.py
+
+trap 'echo "Stopping DoogRadio server..."; sleep 1; exit 0' 2
+
+echo "Starting DoogRadio server...  Press CTRL+C to stop"
+
+while true
+do
+	python radio.py
+	sleep 10
+done
+
+
+
